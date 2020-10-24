ad7fc0aa6923040e7e2469e3cb828297e4e8969f
ACSBSC <36998939+ACSBSC@users.noreply.github.com>
2020-10-23 21:24:10 -0500

    book y calculator

---
+++ clase-23-oct/app/models/Book.js
@@ -0,0 +1,14 @@
+class Book{
+    author(){
+        return this.author
+    }
+    title(){
+        return this.title
+    }
+    subtotal(){
+        return this.subtotal
+    }
+    getTotal(){
+        return this.subtotal*1.16
+    }
+}
--- clase-23-oct/test/my-new-test.js
+++ clase-23-oct/test/my-new-test.js
@@ -1,9 +1,53 @@
 const assert = require('assert');
 
 describe('A series of tests', ()=>{
-    describe('String is correct', ()=>{
-        it('Should say hello world',()=>{
-            assert.strictEqual("hello world", "hello world");
+    
+    describe('Book has correct total', ()=>{
+        it('Should say 116',()=>{
+            let book = new Book("A","A",100);
+            assert.strictEqual(book.getTotal(), book.subtotal*1.16);
+        })
+    })
+    describe('Book has correct full title', ()=>{
+        it('Should say {title} - {author}',()=>{
+            let book = new Book("A","A",100);
+            assert.strictEqual("A - A", book.getFullTitle);
         })
     })
 })
+
+
+
+class Book{
+    constructor(author, title, subtotal){
+        this.author = author
+        this.title = title
+        this.subtotal = subtotal
+    }
+    getTotal(){
+        return this.subtotal*1.16
+    }
+    getFullTitle(){
+        return this.title + " - " + this.author
+    }
+}
+
+class Calculadora{
+    constructor(a, b, x){
+        this.author = author
+        this.title = title
+        this.subtotal = subtotal
+    }
+    getSuma(){
+        return this.a + this.b
+    }
+    getSub(){
+        return this.a - this.b
+    }
+    getScalar(){
+        return this.a - this.b
+    }
+    getDot(){
+        return this.a - this.b
+    }
+}
